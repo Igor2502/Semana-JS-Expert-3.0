24203b47234472638aace2d6d21439c896f5f29a
Igor <igor.gds.00@gmail.com>
2023-06-24 08:57:19 -0300

    Atualizando usuarios na sala e log de atividades

--- client/index.js
+++ client/index.js
@@ -2,6 +2,7 @@ import Events from 'events'
 import TerminalController from "./src/terminalController.js"
 import CliConfig from './src/cliConfig.js'
 import SocketClient from './src/socket.js'
+import EventManager from './src/eventManager.js'
 
 const [nodePath, filePath, ...commands] = process.argv
 const config = CliConfig.parseArguments(commands)
@@ -11,5 +12,15 @@ const componentEmitter = new Events()
 const socketClient = new SocketClient(config)
 await socketClient.initialize()
 
-// const controller = new TerminalController()
-// await controller.initializeTable(componentEmitter)
+const eventManager = new EventManager({ componentEmitter, socketClient })
+const events = eventManager.getEvents()
+socketClient.attachEvents(events)
+
+const data = {
+  roomId: config.room,
+  username: config.username
+}
+eventManager.joinRoomAndWaitForMessages(data)
+
+const controller = new TerminalController()
+await controller.initializeTable(componentEmitter)
